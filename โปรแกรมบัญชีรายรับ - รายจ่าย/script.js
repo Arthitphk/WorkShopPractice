721ffaafc612de73ffca29418568ab025fe00f5b
Arthitphk <128705950+Arthitphk@users.noreply.github.com>
2025-12-29 21:46:02 +0700

    add calculate function

--- โปรแกรมบัญชีรายรับ - รายจ่าย/script.js
+++ โปรแกรมบัญชีรายรับ - รายจ่าย/script.js
@@ -11,17 +11,21 @@ const amount = document.getElementById('amount');
 const datatransaction = [
     {id:1,text:"snack",amount:-100},
     {id:2,text:"room tax",amount:-2200},
-    {id:3,text:"salary",amount:+22000}
+    {id:3,text:"salary",amount:+22000},
+    {id:4,text:"steam sale",amount:-3200},
+    {id:5,text:"win lotaly",amount:+5000}
 ]
+
 
 const transaction = datatransaction;
 
+// วน data
 function init() {
    transaction.forEach(addDataToList)
-    
+   calculateMoney(); 
 }
 
-
+// กรองข้อมูล
 function addDataToList (transaction) {
     const symbol = transaction.amount < 0 ? '-': '+';
     const status = transaction.amount < 0 ? 'minus':'plus';
@@ -32,4 +36,27 @@ function addDataToList (transaction) {
     
 };
 
+function calculateMoney() {
+    const amounts = transaction.map(transaction => transaction.amount)
+    // คำนวณยอดคงเหลือ
+    const total = amounts.reduce((result,item) => (result += item),0).toFixed(2);
+    // คำนวณรายรับ
+    const income = amounts.filter(item=>item > 0).reduce((result,item) => (result += item),0).toFixed(2);
+    // คำนวณรายจ่าย
+    const expense = amounts.filter(item=>item < 0).reduce((result,item) => (result -= item),0).toFixed(2);
+
+    // แสดงผลทางจ
+    balance.innerText =`฿+${total}`
+    money_plus.innerText =`฿+${income}`
+    money_minus.innerText =`฿+${expense}`
+}
+
+
+
+
 init();
+
+
+
+
+ 
